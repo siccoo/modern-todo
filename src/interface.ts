43f79226e91fb86b7e1b0a407057afd0149c75bb
Michael Chilaka <obichilaka@gmail.com>
2022-09-08 14:49:34 +0100

    export 3 props of TodoState

--- src/interface.ts
+++ src/interface.ts
@@ -1,3 +1,12 @@
 export interface TodoState {
-    
+    items: {
+        completed: (string | undefined)[];
+        inProgress: (string | undefined)[];
+        removed: (string | undefined)[];
+    },
+    newItems: string | undefined;
+    views: {
+        open: boolean;
+        selected: string;
+    }
 }
