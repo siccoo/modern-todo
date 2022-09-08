c034cf212aaf55009b5fdbc0a7e15c32dd0c2157
Michael Chilaka <obichilaka@gmail.com>
2022-09-08 07:38:24 +0100

    updated first commit

---
+++ src/components/ListNavigation/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListNavigation = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default ListNavigation;
---
+++ src/components/Todo/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Todo extends React.Component {
+    constructor(props: {}) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className='todo'>Todo</div>
+        )
+    }
+}
+
+export default Todo;
